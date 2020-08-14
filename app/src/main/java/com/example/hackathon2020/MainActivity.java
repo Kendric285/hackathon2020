fbe66ea47ee2d587a46bcecb3144eb8b48418abc
Kendric Terry <kendric285@gmail.com>
2020-08-14 18:23:49 -0400

    edit

--- app/src/main/java/com/example/hackathon2020/MainActivity.java
+++ app/src/main/java/com/example/hackathon2020/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //edit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
