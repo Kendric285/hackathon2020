ef3a133bc3e66931682308210b4ba16f718e5071
infinite2004 <44120607+infinite2004@users.noreply.github.com>
2020-08-22 13:58:02 -0400

    addd food
    add food

--- app/src/main/java/com/example/hackathon2020/FoodTracker.java
+++ app/src/main/java/com/example/hackathon2020/FoodTracker.java
@@ -3,12 +3,53 @@ package com.example.hackathon2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FoodTracker extends AppCompatActivity {
+
+        ArrayList<String> itemlist;
+
+        ArrayAdapter<String> adapter;
+
+        EditText itemText;
+
+        Button addButton;
+
+        ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_tracker);
+
+        lv = (ListView) findViewById(R.id.ListView);
+        itemText= (EditText) findViewById(R.id.addtext);
+        addButton = (Button) findViewById(R.id.addbutton);
+
+        itemlist = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(FoodTracker.this,android.R.layout.simple_list_item_multiple_choice,itemlist);
+
+        View.OnClickListener addlistener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemlist.add(itemText.getText().toString());
+                itemText.setText("");
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+
+
+        addButton.setOnClickListener(addlistener);
+
+        lv.setAdapter(adapter);
     }
 }
