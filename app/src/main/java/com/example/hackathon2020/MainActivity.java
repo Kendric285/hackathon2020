ed7829bec81ebaf0c811575636041a7233ce0b70
Kendric Terry <kendric285@gmail.com>
2020-08-21 23:04:16 -0400

    push

--- app/src/main/java/com/example/hackathon2020/MainActivity.java
+++ app/src/main/java/com/example/hackathon2020/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.hackathon2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button toTimer;
+    Button toFoodTracker;
 
 
 
@@ -19,7 +22,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        toTimer = findViewById(R.id.toTimer);
+        toFoodTracker = findViewById(R.id.toFoodTracker);
+
+
+        toTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toTimer();
+            }
+        });
+
+        toFoodTracker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toTracker();
+            }
+        });
+
+
+
+
+
 
 
     }
+
+    public void toTimer(){
+        Intent toTimer = new Intent(this, WorkoutTimer.class);
+        startActivity(toTimer);
+
+    }
+
+    public void toTracker(){
+        Intent toTracker = new Intent(this, FoodTracker.class);
+        startActivity(toTracker);
+
+    }
+
 }
