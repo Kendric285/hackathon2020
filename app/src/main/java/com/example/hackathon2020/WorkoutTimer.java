2d7887c39300772d245c338116a10262931f44a5
Kendric Terry <kendric285@gmail.com>
2020-08-21 22:40:48 -0400

    new activities

--- app/src/main/java/com/example/hackathon2020/WorkoutTimer.java
+++ app/src/main/java/com/example/hackathon2020/WorkoutTimer.java
@@ -2,24 +2,13 @@ package com.example.hackathon2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-
-
-
+public class WorkoutTimer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_workout_timer);
     }
 }
