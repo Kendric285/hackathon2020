bbbba3200c2e0dbf999dabce6361ac277090f5a8
infinite2004 <44120607+infinite2004@users.noreply.github.com>
2020-08-23 13:04:09 -0400

    layout made
    vids

--- app/src/main/java/com/example/hackathon2020/MainActivity.java
+++ app/src/main/java/com/example/hackathon2020/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Button toFoodTracker;
     Button toCalendar;
     Button toWorkoutsActivity;
-   // Button toWorkoutVids;
+    Button toWorkoutVids;
     ImageView settings;
 
 
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         toTimer = findViewById(R.id.toTimer);
         toFoodTracker = findViewById(R.id.toFoodTracker);
         toWorkoutsActivity = findViewById(R.id.toWorkoutsActivity);
-       // toWorkoutVids = findViewById(R.id.toWorkoutVids);
+        toWorkoutVids = findViewById(R.id.toWorkoutVids);
 
 
 
@@ -99,7 +99,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        toWorkoutVids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toWorkoutVids();
+            }
+        });
 
     }
 
@@ -138,5 +143,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    public void toWorkoutVids(){
+        Intent toWorkoutVids = new Intent(this,Workoutvids.class);
+        startActivity(toWorkoutVids);
+    }
 }
